64440bc5cce7d1903568c63b908e7dbdcbe08100
yunusdal <yunus.dal@inomera.com>
2021-01-07 20:58:17 +0300

    truncate table py script created

---
+++ truncateCassandraTable.py
@@ -0,0 +1,48 @@
+from pymongo import MongoClient
+from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
+import datetime
+import os
+import subprocess
+import sys
+import logging
+
+host1 = 'ip1'
+host2 = 'ip2'
+host3 = 'ip'
+command = 'cd /opt/apache-cassandra-3.10/bin/ ; ./nodetool clearsnapshot msg msg_pack_user'
+
+client = MongoClient()
+client = MongoClient('mongodb://mongo_username:mongopassword@ip:port,ip:port,ip:port/?replicaSet=replica_set_name')
+
+auth_provider = PlainTextAuthProvider(username='', password='')
+cluster = Cluster(["ip"], port=9042, auth_provider = auth_provider)
+
+log = logging.getLogger()
+log.setLevel('DEBUG')
+handler = logging.StreamHandler()
+handler.setFormatter(logging.Formatter("%(asctime)s [%(levelname)s] %(name)s: %(message)s"))
+log.addHandler(handler)
+
+def main():
+
+    db = client.nm 
+    ISODate = datetime.datetime.today().strftime('%Y/%m/%d')
+    count = db.msgPacks.find({"send":{"$gt":ISODate}}).count()
+
+    if count == 0:
+
+        log.info("msgPack collection count is zero...")
+        session = cluster.connect()
+        log.info("truncating msg_pack_user table...")
+        session.execute("truncate msg.msg_pack_user")
+        log.info("closing cassandra connection")
+        session.close()
+
+        log.info("clear snaphosts on remote hosts...")
+        subprocess.Popen(["ssh", "%s" % host1, command])
+        subprocess.Popen(["ssh", "%s" % host2, command])
+        subprocess.Popen(["ssh", "%s" % host3, command])
+    
+if __name__ == "__main__":
+    main()

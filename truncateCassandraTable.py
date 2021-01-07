58667985b31ca9c5b9d5c0c465b8d11431360fe8
yunusdal <yunus.dal@inomera.com>
2021-01-07 21:13:37 +0300

    added comment

--- truncateCassandraTable.py
+++ truncateCassandraTable.py
@@ -1,3 +1,6 @@
+# @author yunus dal
+# this script; if mongodb msgPack collection count is zero truncate cassandra msg_pack_user table
+
 from pymongo import MongoClient
 from cassandra.cluster import Cluster
 from cassandra.auth import PlainTextAuthProvider
@@ -7,23 +10,28 @@ import subprocess
 import sys
 import logging
 
+# ********* remote server infos and cassandra installation path for clear snapshots after truncate table *********
 host1 = 'ip1'
 host2 = 'ip2'
 host3 = 'ip'
 command = 'cd /opt/apache-cassandra-3.10/bin/ ; ./nodetool clearsnapshot msg msg_pack_user'
 
+# ********* mongodb connection *********
 client = MongoClient()
 client = MongoClient('mongodb://mongo_username:mongopassword@ip:port,ip:port,ip:port/?replicaSet=replica_set_name')
 
+# ********* cassandra connection *********
 auth_provider = PlainTextAuthProvider(username='', password='')
 cluster = Cluster(["ip"], port=9042, auth_provider = auth_provider)
 
+# ********* logging *********
 log = logging.getLogger()
 log.setLevel('DEBUG')
 handler = logging.StreamHandler()
 handler.setFormatter(logging.Formatter("%(asctime)s [%(levelname)s] %(name)s: %(message)s"))
 log.addHandler(handler)
 
+# ********* main class *********
 def main():
 
     db = client.nm 
@@ -43,6 +51,8 @@ def main():
         subprocess.Popen(["ssh", "%s" % host1, command])
         subprocess.Popen(["ssh", "%s" % host2, command])
         subprocess.Popen(["ssh", "%s" % host3, command])
+    else:
+        log.info("msgPack collection count is not zero...")
     
 if __name__ == "__main__":
     main()
